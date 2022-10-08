0a98ba4cf49d1371c0e7d2ee4303e55c5fe74237
Hadi Rezaee <se.hadirezaee@gmail.com>
2022-10-08 14:17:57 +0200

    add specimen city generator

---
+++ DemoApi.Tests/Infrastructure/DataGenerators/CityStringGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using AutoFixture.Kernel;
+
+namespace DemoApi.Tests.Infrastructure.DataGenerators;
+
+public class CityStringGenerator : ISpecimenBuilder
+{
+    public object Create(object request, ISpecimenContext context)
+    {
+        if (request is not PropertyInfo propertyInfo)
+        {
+            return new NoSpecimen();
+        }
+
+        var isCountryCode = propertyInfo.Name.Contains("City");
+        var isStringProperty = propertyInfo.PropertyType == typeof(string);
+
+        if (isCountryCode && isStringProperty)
+        {
+            return "Vienna";
+        }
+
+        return new NoSpecimen();
+    }
+    
+}
---
+++ DemoApi.Tests/ModelTests/AddressTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture;
+using DemoApi.Models;
+using DemoApi.Tests.Infrastructure.DataGenerators;
+using FluentAssertions;
+
+namespace DemoApi.Tests.ModelTests;
+
+public class AddressTests
+{
+    private readonly Fixture _fixture;
+
+    public AddressTests()
+    {
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public void Address_WithManualCustomization()
+    {
+        //arrange
+        _fixture.Customize<Address>(u =>
+            u.With(x => x.CountryCode, "AT")
+        );
+
+        //act
+        var address = _fixture.Create<Address>();
+
+        //assert
+        address.CountryCode.Should().Be("AT");
+    }
+
+    [Fact]
+    public void Address_WithSpecimenCustomization()
+    {
+        //arrange
+        _fixture.Customizations.Add(new CityStringGenerator());
+
+        //act
+        var addresses = _fixture.CreateMany<Address>(6);
+       
+
+        //assert
+        addresses.Should().OnlyContain(x => x.City == "Vienna");
+    }
+}
