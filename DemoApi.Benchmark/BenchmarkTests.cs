dda31f7b4a6f5a5340802d33d61d629e194501ef
Hadi Rezaee <mohammadhadi.rezaee@post.at>
2022-10-14 15:07:17 +0200

    ad benchmark model

---
+++ DemoApi.Benchmark/BenchmarkTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture;
+using BenchmarkDotNet.Attributes;
+
+namespace DemoApi.Benchmark;
+
+
+[MemoryDiagnoser]
+public class BenchmarkTests
+{
+    [Benchmark]
+    public void UnitTest_With_Manual_Object()
+    {
+        var employee = new Employee()
+        {
+            Id = 2,
+            Birthday = DateTime.Now,
+            Gender = Gender.Female,
+            Name = "some name",
+            Lastname = "some last name",
+            
+        };
+    }
+
+    [Benchmark]
+    public void UnitTest_With_AutoFixture()
+    {
+        var fixture = new Fixture();
+        var employee = fixture.Create<Employee>();
+    }
+    
+}
---
+++ DemoApi.Benchmark/TestModel.cs
@@ -0,0 +1,47 @@
+﻿namespace DemoApi.Benchmark;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public DateTime Birthday { get; set; }
+    public List<Address> Addresses { get; set; }
+    public Gender Gender { get; set; }
+    public Department Department { get; set; }
+}
+
+public class Department
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<Section> Sections { get; set; }
+}
+
+public class Section
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public enum Gender
+{
+    Female,
+    Male,
+    Other
+}
+
+public class Address
+{
+    public Guid Id { get; set; }
+    public string Street { get; set; }
+    public string Country { get; set; }
+    public string ZipCode { get; set; }
+    public AddressType AddressType { get; set; }
+}
+
+public enum AddressType
+{
+    Home,
+    Work
+}
